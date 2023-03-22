95f76d1961c8306b777e7dc3bdb44886c0529b33
Kumiki007 <128634483+Kumiki007@users.noreply.github.com>
2023-03-22 19:41:31 +0100

    Add files via upload

---
+++ KursZrobione.sh
@@ -0,0 +1,22 @@
+git commit
+git branch myBranch
+git commit
+git checkout myBranch
+git merge bugFix
+git checkout bugFix
+git merge main
+git rebase main
+git rebase bugFix
+git checkout C1 
+git checkout main
+git checkout C1 
+git checout main^
+git checkout HEAD~4
+git branch -f main HEAD~3
+git reset HEAD~1
+git revert HEAD
+git cherry-pick C2 C4
+git rebase -i HEAD~4
+git cherry-pick C2
+git tag v1 C1
+git tag v2 C3
